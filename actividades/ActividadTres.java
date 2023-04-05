896d8dcec1c214e04d657086caa11931d8c12b15
Pablo Nuñez <polnunez@outlook.com>
2023-04-04 22:27:20 -0300

    Metodo para comprobar repetidos

--- actividades/ActividadTres.java
+++ actividades/ActividadTres.java
@@ -4,15 +4,27 @@ public class ActividadTres {
     public static int[] cargarArregloRandom(int cantidad) {
         int[] numerosRandom = new int[cantidad];
 
-        for (int i = 0; i <= numerosRandom.length; i++) {
-            numerosRandom[i] = ((int) Math.random() * 100);
+        for (int i = 0; i < numerosRandom.length; i++) {
+            numerosRandom[i] = (int) (Math.random() * 100);
+            if (checkRepetidos(numerosRandom)) {
+                i--;
+            }
+        }
+
+        return numerosRandom;
+    }
+
+    public static boolean checkRepetidos(int[] numeros) {
+        boolean hayRepetido = false;
+
+        for (int i = 0; i < numeros.length; i++) {
             for (int j = 0; j < i; j++) {
-                if (numerosRandom[i] == numerosRandom[j]) {
-                    i--;
+                if (numeros[i] == numeros[j]) {
+                    hayRepetido = true;
                     break;
                 }
             }
         }
-        return numerosRandom;
+        return hayRepetido;
     }
 }
